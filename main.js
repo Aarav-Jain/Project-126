2eb8d0b4b154bc360b987bfc3bf3476cb43153a5
Aarav Jain <85875038+Aarav-Jain@users.noreply.github.com>
2023-10-26 18:38:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+
+}
+
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+    
+}
+
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialised');
+}
